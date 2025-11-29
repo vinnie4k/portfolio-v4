2abea162f47e0f3752126c277782155bd216ba24
Vin Bui <b.vinhan01@gmail.com>
2025-11-29 11:24:28 -0800

    feat: add posthog for analytics tracking

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,13 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
 import { setupRouterSsrQueryIntegration } from "@tanstack/react-router-ssr-query";
+import { PostHogProvider } from "posthog-js/react";
 import { routeTree } from "./routeTree.gen";
+
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: "2025-05-24",
+} as const;
 
 export const getRouter = () => {
   const queryClient = new QueryClient();
@@ -13,9 +19,14 @@ export const getRouter = () => {
     defaultPreload: "intent",
     Wrap: (props: { children: React.ReactNode }) => {
       return (
-        <QueryClientProvider client={queryClient}>
-          {props.children}
-        </QueryClientProvider>
+        <PostHogProvider
+          apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+          options={options}
+        >
+          <QueryClientProvider client={queryClient}>
+            {props.children}
+          </QueryClientProvider>
+        </PostHogProvider>
       );
     },
   });
