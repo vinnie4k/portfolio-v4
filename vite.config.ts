1dd4d5659f34b9b6de138daf2270c2face0724fd
Vin Bui <b.vinhan01@gmail.com>
2025-11-29 12:31:17 -0800

    feat: add deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
@@ -12,8 +13,10 @@ const config = defineConfig({
     }),
     tailwindcss(),
     tanstackStart(),
+    nitro(),
     viteReact(),
   ],
+  nitro: {},
 });
 
 export default config;
