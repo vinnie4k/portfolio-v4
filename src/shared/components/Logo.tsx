23f2e51e3336d92d4b9c3d13bf7bcad397aec10d
Vin Bui <b.vinhan01@gmail.com>
2025-12-01 01:50:47 -0800

    fix: fix dark mode issues with logo

--- src/shared/components/Logo.tsx
+++ src/shared/components/Logo.tsx
@@ -6,9 +6,7 @@ export const Logo = ({ onClick }: { onClick?: () => void }) => {
       onClick={onClick}
       className="clickable bg-transparent border-none p-0"
     >
-      <h1 className="text-2xl font-script text-gray-900 dark:text-gray-100">
-        {dnt("Vin Bui")}
-      </h1>
+      <h1 className="text-2xl font-script text-gray-900">{dnt("Vin Bui")}</h1>
     </button>
   );
 };
